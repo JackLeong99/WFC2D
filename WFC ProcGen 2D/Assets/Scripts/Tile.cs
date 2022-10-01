4d18edaf623f299e2f96635bcd989e5700f7db68
Awoo_ <jackleong875@gmail.com>
2022-10-02 00:27:03 +1000

    prototyping

---
+++ WFC ProcGen 2D/Assets/Scripts/Tile.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tile/New Tile")]
+
+public class Tile : ScriptableObject
+{
+    public float size;
+    public Vector2Int coordinates;
+    public GameObject sprite;
+    public List<Edge> edges;
+
+    public void RotateRight() 
+    {
+        var last = edges[edges.Count - 1];
+        edges.RemoveAt(edges.Count - 1);
+        edges.Insert(0, last);
+        sprite.transform.Rotate(0.0f, 0.0f, -90.0f);
+    }
+}
+
+public struct Edge //this cant be a sctruct because it doesnt render in the inspector so defining edges is gonna be so painful
+{
+    public List<char> code;
+
+    public bool Compatible(Edge e) 
+    {
+        List<char> temp = e.code;
+        temp.Reverse();
+        switch (true) 
+        {
+            case bool x when temp == this.code:
+                return true;
+            default:
+                return false;
+        }
+    }
+}
